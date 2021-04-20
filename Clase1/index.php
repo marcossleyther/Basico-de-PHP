f6d9c4755682f19a5310318da9fe56d2a8d76d8f
Sleyther <marcossleyther@gmail.com>
2021-04-19 20:27:13 -0600

    Imprimir las variables

---
+++ Clase1/index.php
@@ -0,0 +1,15 @@
+<?php
+
+ //tipos de variables
+ $entero = 25;
+ $cadena = 'Marcos';
+ $flotante = 32.325;
+
+ //Para imprimir
+echo "Mi nombre es " . $cadena;
+echo " .Un simple entero ".$entero;
+echo " y uno con decimales " .$flotante;
+
+
+
+?>
